33d627c9edbdd87ccd16d1574f6bda58580e1d89
J. R. Okajima <hooanon05g@gmail.com>
2015-03-30 07:49:30 +0900

    aufs: export via NFS 1/2, body
    Implement exporting via NFS.
    The file handle is rather large (40 bytes at most + the file handle on a
    branch).
    The non-virtual filesystems can use an anonymous (disconnected) dentry
    as long as the inode is identifed, but aufs needs a dentry with dinfo
    which is usually contructed. So aufs has to find or generate the normal
    dentry from the file handle in decoding.
    If both of the dentry and the inode which are correspondging the file
    handle are still in cache, then they are returnd immediately. Otherwise
    aufs has to find the cached parent dir from the file handle. If the
    parent dir is not cached either, the aufs tries
    - decode the branch fs's file handle
    - generate the path of the parent dir on the branch
    - convert the branch path to aufs's path
    - lookup the inode number under the aufs's path
    The last one is the slowest case.
    
    See also the document in later commit.
    This is compiled only when CONFIG_AUFS_EXPORT is enabled.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- fs/aufs/super.h
+++ fs/aufs/super.h
@@ -143,6 +143,12 @@ struct au_sbinfo {
 	/* reserved for future use */
 	/* unsigned long long	si_xib_limit; */	/* Max xib file size */
 
+#ifdef CONFIG_AUFS_EXPORT
+	/* i_generation */
+	struct file		*si_xigen;
+	atomic_t		si_xigen_next;
+#endif
+
 	/* vdir parameters */
 	unsigned long		si_rdcache;	/* max cache time in jiffies */
 	unsigned int		si_rdblk;	/* deblk size */
@@ -271,6 +277,32 @@ static inline struct au_sbinfo *au_sbi(struct super_block *sb)
 {
 	return sb->s_fs_info;
 }
+
+/* ---------------------------------------------------------------------- */
+
+#ifdef CONFIG_AUFS_EXPORT
+int au_test_nfsd(void);
+void au_export_init(struct super_block *sb);
+void au_xigen_inc(struct inode *inode);
+int au_xigen_new(struct inode *inode);
+int au_xigen_set(struct super_block *sb, struct file *base);
+void au_xigen_clr(struct super_block *sb);
+
+static inline int au_busy_or_stale(void)
+{
+	if (!au_test_nfsd())
+		return -EBUSY;
+	return -ESTALE;
+}
+#else
+AuStubInt0(au_test_nfsd, void)
+AuStubVoid(au_export_init, struct super_block *sb)
+AuStubVoid(au_xigen_inc, struct inode *inode)
+AuStubInt0(au_xigen_new, struct inode *inode)
+AuStubInt0(au_xigen_set, struct super_block *sb, struct file *base)
+AuStubVoid(au_xigen_clr, struct super_block *sb)
+AuStub(int, au_busy_or_stale, return -EBUSY, void)
+#endif /* CONFIG_AUFS_EXPORT */
 
 /* ---------------------------------------------------------------------- */
 
