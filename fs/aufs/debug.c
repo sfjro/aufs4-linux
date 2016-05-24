ae4bd3101d8e0bb0bc02c78165287e882bbc786c
J. R. Okajima <hooanon05g@gmail.com>
2016-05-24 23:48:03 +0900

    aufs: convert nw_len into percpu_counter
    With a little hope to gain better performance.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>
    (cherry picked from commit 68e2ed22c338bd7a589ed9f558f07df3cd1fe1fb)

--- fs/aufs/debug.c
+++ fs/aufs/debug.c
@@ -340,8 +340,9 @@ void au_dpri_sb(struct super_block *sb)
 	sbinfo = au_sbi(sb);
 	if (!sbinfo)
 		return;
-	dpri("nw %d, gen %u, kobj %d\n",
-	     atomic_read(&sbinfo->si_nowait.nw_len), sbinfo->si_generation,
+	dpri("nw %lld, gen %u, kobj %d\n",
+	     percpu_counter_sum(&sbinfo->si_nowait.nw_len),
+	     sbinfo->si_generation,
 	     atomic_read(&sbinfo->si_kobj.kref.refcount));
 	for (bindex = 0; bindex <= sbinfo->si_bbot; bindex++)
 		do_pri_br(bindex, sbinfo->si_branch[0 + bindex]);
