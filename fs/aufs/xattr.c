b796860f2fbdf80291d4fcd248311fc60840e9e7
J. R. Okajima <hooanon05g@gmail.com>
2017-01-23 03:36:33 +0900

    aufs: for v4.9, support posix acl
    The commit
    	3801e27 2016-12-08 aufs: for v4.9-rc1, support new xattr scheme
    was not enough, and supporting posix acl was missing.
    
    Reported-by: Justin Cormack <justin.cormack@docker.com>
    See-also: https://github.com/docker/docker/issues/30245#issuecomment-274152452
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- fs/aufs/xattr.c
+++ fs/aufs/xattr.c
@@ -19,6 +19,8 @@
  * handling xattr functions
  */
 
+#include <linux/fs.h>
+#include <linux/posix_acl_xattr.h>
 #include <linux/xattr.h>
 #include "aufs.h"
 
@@ -322,7 +324,11 @@ static const struct xattr_handler au_xattr_handler = {
 };
 
 static const struct xattr_handler *au_xattr_handlers[] = {
-	&au_xattr_handler,
+#ifdef CONFIG_FS_POSIX_ACL
+	&posix_acl_access_xattr_handler,
+	&posix_acl_default_xattr_handler,
+#endif
+	&au_xattr_handler, /* must be last */
 	NULL
 };
 
